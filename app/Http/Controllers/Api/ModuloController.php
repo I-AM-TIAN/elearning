759f0dc09ba13f951a813d2ed1e5eb0d90b60842
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-09-17 16:46:56 -0500

    modificacion api para buscar por módulos

--- app/Http/Controllers/Api/ModuloController.php
+++ app/Http/Controllers/Api/ModuloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Curso;
 use App\Models\Leccion;
 use App\Models\Modulo;
 use Illuminate\Http\Request;
@@ -31,11 +32,22 @@ class ModuloController extends Controller
     public function show(string $id)
     {
         //Muestra la información de un módulo asociado a sus lecciones
+
         $modulo = Modulo::where('uuid', $id)->first();
 
         if (!$modulo) {
             $data = [
                 'message' => "No se ha encontrado el modulo",
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $curso = Curso::where('id', $modulo->id_curso)->first();
+
+        if (!$curso) {
+            $data = [
+                'message' => "No se ha encontrado el curso",
                 'status' => 400
             ];
             return response()->json($data, 400);
@@ -52,6 +64,7 @@ class ModuloController extends Controller
         }
 
         $data = [
+            'curso' => $curso,
             'modulo' => $modulo,
             'lecciones' => $lecciones
         ];
